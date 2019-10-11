30e1fb8afc84ea5921714f8e8a964cfe08a8105b
Chikouni <louisdoubi@gmail.com>
2019-10-11 11:58:50 +0200

    DEV/ WIP

--- projetapp-vue/src/components/Event.vue
+++ projetapp-vue/src/components/Event.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="hello" id="app">
-    <div class='btn'>
-      <button v-on:click='fullInfo = !fullInfo'>Afficher toute les infos</button>
-    </div>
-    <ul>
-    <li v-for="info in infos">
-        <h1>{{ info.fields.nom }}</h1>
-
-        <a v-bind:href="info.fields.lien_agenda">
-          <img v-bind:src="info.fields.media_1" :class="{ today : info.fields.date === currentDate }"/>
-        </a>
+    <div class="container">
+    <b-card v-for="info in infos"
+      style="max-width: 30rem;"
+      class="mb-2"
+      v-bind:img-src="info.fields.media_1"
+      v-bind:title="info.fields.nom"
+      :class="{ today : info.fields.date === currentDate }"
+      border-variant="dark"
+      align="center"
+      footer="Ah"
+      >
+      <b-card-text>
         <p>
           <span class='fieldInfo'>Adresse :</span>
            {{ info.fields.adresse }} {{  info.fields.code_postal }} {{  info.fields.ville }}
+        </p>
+        <p>
+          <span class='fieldInfo'>Date :</span>
+          {{ info.fields.date | format }}
         </p>
         <p v-if='fullInfo'>
           <span class='fieldInfo'>Places :</span>
           <span v-if='info.fields.precisions_tarifs'>{{ info.fields.precisions_tarifs }}</span>
           <span v-else>Gratuit</span>
         </p>
-    </li>
-</ul>
+        <b-button v-on:click='fullInfo = !fullInfo'>Afficher toute les infos</b-button>
+      </b-card-text>
+    </b-card>
+</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios' // import lib
 import moment from 'moment'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+require('moment/locale/fr')
 
 var apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&facet=emetteur&facet=rubrique&facet=lieu&facet=ville'
 export default{
@@ -43,41 +54,19 @@ export default{
       .get(apiURL)
       .then(response => (this.infos = response.data.records))
   },
-  filters: { // Pas eu le temps de faire le filtre / peut etre avec computed
+  filters: {
+    format: function (value) {
+      return moment(value).format('dddd LL')
+    }
   }
 }
 </script>
 
 <style>
-h1, h2 {
-  font-weight: normal;
-  max-width: 50%;
-  margin: auto;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
-
-img.today{
+.card.today{
   border: #4db12c solid 5px;
 }
-
 span.fieldInfo{
   font-weight: 600;
-}
-p{
-  max-width: 50%;
-  margin: auto;
 }
 </style>
